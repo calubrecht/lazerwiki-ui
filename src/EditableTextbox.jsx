c6ab9ee8051c5bd3ce92248b0541c6c93e964041
Chad Lubrecht <chad@calubrecht.us>
2023-11-11 21:02:11 -0500

    Add missing files

---
+++ src/EditableTextbox.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+import './EditableTextbox.css';
+
+export default class EditableTextbox extends Component
+{
+  constructor(props) {
+    super(props);
+    this.state = {text: props.text};
+    this.props.registerTextCB(() => this.state.text);
+  }
+
+  render()
+  {
+    return <div><textarea rows="25" cols="80" name="message" className="message" value={this.state.text} onChange={ev => this.onChangeText(ev) }></textarea></div>
+  }
+
+  onChangeText(ev)
+  {
+    this.setState({text: ev.target.value});
+  }
+
+  submit(ev)
+  {
+    ev.preventDefault();
+    this.props.doSubmit(this.state.text);
+  }
+  
+  cancel(ev)
+  {
+    ev.preventDefault();
+    this.setState({text:null});
+    this.props.doCancel()
+  }
+}
