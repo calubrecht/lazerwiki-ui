011ec5e29d887dcc44341c829975e3322aa12e59
Chad Lubrecht <chad@calubrecht.us>
2023-12-10 21:53:32 -0500

    Render user define links from _meta:toolbar as part of the toolbar

--- src/Toolbar.jsx
+++ src/Toolbar.jsx
@@ -2,12 +2,32 @@ import MediaFrame from './MediaFrame';
 import PageFrame from './PageFrame';
 import DrawerLink from './DrawerLink';
 import {useState} from 'react';
+import {useEffect} from 'react';
+import {instance as DS_instance} from './svc/DataService';
+import HTMLReactParser from 'html-react-parser';
 
-  export default function Toolbar() {
-    let [showMedia, setShowMedia] = useState(false);
+
+
+export default function Toolbar() {
+    let [userToolbar, setUserToolbar] = useState('');
+    useEffect( () => {
+      DS_instance().fetchPage("_meta:toolbar").then((pageData) => {
+        if (!pageData.flags.exists) {
+          console.log("No user toolbar info found. To add your own entries, create a page name _meta:toolbar");
+          return;
+        }
+        setUserToolbar(pageData.rendered);
+      }).catch((e) =>  {
+        if (e.message === "Not Found") {
+          return;
+        }
+        console.log(e);
+      });
+    }, []);
     return <div className="toolbar">
        <div className="bold">Toolbar</div>
        <div><a href="/">Home</a></div>
+       {HTMLReactParser(userToolbar)}
        <DrawerLink title="Media List" component={MediaFrame} />
        <DrawerLink title="Page List" component={PageFrame} />
       </div>
