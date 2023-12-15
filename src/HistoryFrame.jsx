2647951602aee151b61c3a864ad942a85a78ea25
Chad Lubrecht <chad@calubrecht.us>
2023-12-15 14:30:25 -0500

    Enable selection and diff button

--- src/HistoryFrame.jsx
+++ src/HistoryFrame.jsx
@@ -30,7 +30,11 @@ export default class HistoryFrame extends Component
   }
  
   fetchPageHistory() {
-    this.data.fetchPageHistory(this.pageName).then(data => this.setState({historyList:data, loading:false}));
+    this.data.fetchPageHistory(this.pageName).then(data =>
+      {
+        let max = Math.max(...data.map( d => d.revision));
+        this.setState({historyList:data, loading:false, maxRevision:max})
+      });
   }
 
 
@@ -41,21 +45,61 @@ export default class HistoryFrame extends Component
       <div onClick={() => this.props.doClose()} className="close">X</div>
         <h2 className="title">History - {this.pageName}</h2>
         {this.renderList()}
+        <div><button onClick={() => this.doDiff()} disabled={!this.state.startSelect || !this.state.endSelect} >View Diff</button></div>
     </div>;
   }
 
-  renderLinkName(p) {
+  renderLinkName(p, first) {
+    if (first) {
+      return  "Current - Modified " + p.modified + " - by " + p.modifiedBy;
+    }
     return  'Revision ' + p.revision + " - Modified " + p.modified + " - by " + p.modifiedBy;
+  }
+
+  renderDiffSelections(revision) {
+    let imgType = this.state.startSelect == revision ? "startSelect.png" : (this.state.endSelect == revision ? "endSelect.png" : "noSelect.png");
+    return <span className="diffSelections"><img src={"/_resources/" + imgType} onClick={() => this.doSelect(revision)} /></span>
   }
 
   renderList() {
     let pages = this.state.historyList;
     if (!pages || pages.length == 0) {
-      return <div>No links to this page</div>;
+      return <div>Could not find history for this page</div>;
     }
+    let first = true;
     return (<div className="historyList">
-        {pages.toReversed().map( p => <div key={p.revision}>{this.renderLinkName(p)}</div>)}
+        {pages.toReversed().map( p => {
+          let name = this.renderLinkName(p, first);
+          first = false;
+          return <div key={p.revision}>{this.renderDiffSelections(p.revision)}{name}</div>;})}
 
       </div>);
+  }
+
+  doSelect(revision) {
+    if (revision == this.state.maxRevision) {
+      this.setState({endSelect: revision});
+      return;
+    }
+    if (!this.state.startSelect) {
+      this.setState({endSelect: this.state.maxRevision, startSelect: revision});
+      return; 
+    }
+    if (revision == this.state.endSelect){ 
+      this.setState({endSelect: this.state.maxRevision, startSelect: revision});
+      return
+    }
+    if (revision >  this.state.endSelect) {
+      this.setState({endSelect: revision});
+      return;
+    }
+    if (revision <= this.state.startSelect) {
+      this.setState({startSelect: revision});
+      return;
+    }
+    this.setState({endSelect:revision});
+  }
+
+  doDiff() {
   }
 }
