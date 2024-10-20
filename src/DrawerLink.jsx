7a2092868444785173ea8a82ca413eba67594b6c
Chad Lubrecht <chad@calubrecht.us>
2024-10-20 10:38:15 -0400

    Add icons to indicate drawer pulls

--- src/DrawerLink.jsx
+++ src/DrawerLink.jsx
@@ -12,5 +12,6 @@ export default function DrawerLink(props) {
    let [showComponent, setShowComponent] = useState(false);
    allDrawerCloseFuncs[props.title] = () => setShowComponent(false);
    let className = props.extraClasses ? "drawer " + props.extraClasses : "drawer";
-   return <div className={className}><button className="drawerPull button-unstyled" onClick={() => {closeAll(); setShowComponent(!showComponent);}}>{props.title}</button> {showComponent && <props.component doClose={() => setShowComponent(false)} initData={props.initData}/>}</div>;
+   let buttonClass = "drawerPull button-unstyled" + (showComponent ? " open" : "");
+   return <div className={className}><button className={buttonClass} onClick={() => {closeAll(); setShowComponent(!showComponent);}}>{props.title}</button> {showComponent && <props.component doClose={() => setShowComponent(false)} initData={props.initData}/>}</div>;
 }
