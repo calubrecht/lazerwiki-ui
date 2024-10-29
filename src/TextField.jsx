2f83aa62555e057202ce476390475a0f1755613d
Chad Lubrecht <chad@calubrecht.us>
2024-10-28 21:47:46 -0400

    Fix duplicate ids in TextFields

--- src/TextField.jsx
+++ src/TextField.jsx
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 export default function TextField(props) {
   let fieldType = props.isPassword ? "password" : "text";
   let className = props.className ? props.className : "TextField";
+  let id = props.name + (Math.random()*10000);
 
-  return <div className={className}><label htmlFor={props.name} className="textFieldLabel">{props.label}</label><input type={fieldType} placeholder={props.name} id={props.name} onChange={evt => props.onChange(evt.target.value, props.varName)} disabled={props.disabled} autoFocus={props.autofocus} value={props.value}></input></div>;
+  return <div className={className}><label htmlFor={id} className="textFieldLabel">{props.label}</label><input type={fieldType} placeholder={props.name} id={id} onChange={evt => props.onChange(evt.target.value, props.varName)} disabled={props.disabled} autoFocus={props.autofocus} value={props.value}></input></div>;
 }
