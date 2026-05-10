972cf4dbdf24333536d071dfa0bdb37c7664aa5b
Chad Lubrecht <chad@calubrecht.us>
2026-05-10 12:31:16 -0400

    Allow override proxy location w/ env variable

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), "API_HOST");
+  const apiServer = env.API_HOST ? env.API_HOST : "app-server";
+  console.log("Proxy forwarding to " + apiServer);
+  return {
   plugins: [react()],
   server: {
     host: true,
@@ -11,24 +15,24 @@ export default defineConfig({
     proxy: {
       '/app':
       {
-          target: 'http://app-server:8080',
+          target: 'http://' + apiServer + ':8080',
           rewrite: (path) => path.replace(/^\/app/, ''),
       },
       '/_media':
       {
-          target: 'http://app-server:8080',
+          target: 'http://' + apiServer + ':8080',
       },
       '/_resources':
       {
-          target: 'http://app-server:8080',
+          target: 'http://' + apiServer + ':8080',
       },
       '/sitemap.xml':
       {
-          target: 'http://app-server:8080',
+          target: 'http://' + apiServer + ':8080',
       },
     }
   },
   build: {
     outDir: "build"
   }
-})
+}})
